73b36ce7f4a4a58c321ed8554a4c019d54bc5e1a
Ba <luckyba2010@gmail.com>
2021-07-12 13:52:47 +0700

    Initial commit

---
+++ app/src/main/java/com/luckyba/myfile/app/AppController.java
@@ -0,0 +1,97 @@
+package com.luckyba.myfile.app;
+
+import android.app.Application;
+
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.StandardExceptionParser;
+import com.google.android.gms.analytics.Tracker;
+import com.luckyba.myfile.activity.MainActivity;
+import com.luckyba.myfile.helper.AnalyticsTrackers;
+import com.luckyba.myfile.model.MediaFileListModel;
+
+import java.util.ArrayList;
+
+public class AppController extends Application {
+    private static AppController mInstance;
+    private ArrayList<MediaFileListModel>  mediaFileListModelArrayList;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+        AnalyticsTrackers.initialize(this);
+        AnalyticsTrackers.getInstance().get(AnalyticsTrackers.Target.APP);
+    }
+
+    public static synchronized AppController getInstance() {
+        return mInstance;
+    }
+
+
+    public void setButtonBackPressed(MainActivity.ButtonBackPressListener listener) {
+        MainActivity.buttonBackPressListener=listener;
+    }
+
+    public void setMediaFileListArrayList(ArrayList<MediaFileListModel> mediaFileListArrayList) {
+        this.mediaFileListModelArrayList = mediaFileListArrayList;
+    }
+
+    public ArrayList<MediaFileListModel> getMediaFileListModeLArray() {
+        return mediaFileListModelArrayList;
+    }
+
+    public synchronized Tracker getGoogleAnalyticsTracker() {
+        AnalyticsTrackers analyticsTrackers = AnalyticsTrackers.getInstance();
+        return analyticsTrackers.get(AnalyticsTrackers.Target.APP);
+    }
+
+    /***
+     * Tracking screen view
+     *
+     * @param screenName screen name to be displayed on GA dashboard
+     */
+    public void trackScreenView(String screenName) {
+        Tracker t = getGoogleAnalyticsTracker();
+
+        // Set screen name.
+        t.setScreenName(screenName);
+
+        // Send a screen view.
+        t.send(new HitBuilders.ScreenViewBuilder().build());
+
+        GoogleAnalytics.getInstance(this).dispatchLocalHits();
+    }
+
+    /***
+     * Tracking exception
+     *
+     * @param e exception to be tracked
+     */
+    public void trackException(Exception e) {
+        if (e != null) {
+            Tracker t = getGoogleAnalyticsTracker();
+
+            t.send(new HitBuilders.ExceptionBuilder()
+                    .setDescription(
+                            new StandardExceptionParser(this, null)
+                                    .getDescription(Thread.currentThread().getName(), e))
+                    .setFatal(false)
+                    .build()
+            );
+        }
+    }
+
+    /***
+     * Tracking event
+     *
+     * @param category event category
+     * @param action   action of the event
+     * @param label    label
+     */
+    public void trackEvent(String category, String action, String label) {
+        Tracker t = getGoogleAnalyticsTracker();
+
+        // Build and send an Event.
+        t.send(new HitBuilders.EventBuilder().setCategory(category).setAction(action).setLabel(label).build());
+    }
+}
---
+++ app/src/main/java/com/luckyba/myfile/model/TextEditorOptionsModel.java
@@ -0,0 +1,21 @@
+package com.luckyba.myfile.model;
+
+
+public class TextEditorOptionsModel {
+    private int textSize;
+
+    public TextEditorOptionsModel(int textSize) {
+        this.textSize = textSize;
+    }
+
+    public TextEditorOptionsModel() {
+    }
+
+    public int getTextSize() {
+        return textSize;
+    }
+
+    public void setTextSize() {
+        this.textSize = 30;
+    }
+}
